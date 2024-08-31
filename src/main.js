fc2b076668674ca856734e32f4730cb9ceb7ed98
NdiayeOusmaneCamara <167888415+NdiayeOusmanaCamara@users.noreply.github.com>
2024-08-31 21:47:44 +0000

    udapte index

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap/dist/js/bootstrap.js"
-import "./assets/css/style.css"
+// import "./assets/css/style.css"
 
 import router from '@router'
 import { createApp } from 'vue'
